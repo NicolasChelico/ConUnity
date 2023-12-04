830daf1ef2b7f1c37a63566efa9a8082bc07e503
Houssam Righi <hrighi01@gmail.com>
2023-12-04 14:16:25 -0500

    Fix formatting in sideDrawer.js

--- client/src/Components/sideDrawer.js
+++ client/src/Components/sideDrawer.js
@@ -1,34 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './sideDrawer.css'
+import "./sideDrawer.css";
 import { IoClose } from "react-icons/io5";
 
 const SideDrawer = (props) => {
-
-
-    return(
-
-        <div className="side__drawer">
-            <div className="exit__button">
-                <button className="btn" onClick={props.onClick}>
-                        <IoClose size={20}/>
-                        </button> 
-            </div>
-                <div className ="close__button__drawer">
-                    <h5>CONUNITY</h5>
-                    <div className="leave__review__button">
-                        <button><Link to="/ReviewCourse">Leave a Review!</Link></button>
-                    </div>
-                </div>
-               
-                
-            <ul>
-               {props.children}
-            </ul>
+  return (
+    <div className="side__drawer">
+      <div className="exit__button">
+        <button className="btn" onClick={props.onClick}>
+          <IoClose size={20} color="#FFFFFF" />
+        </button>
+      </div>
+      <div className="close__button__drawer">
+        <h5>CONUNITY</h5>
+        <div className="leave__review__button">
+          <button>
+            <Link to="/ReviewCourse">Leave a Review!</Link>
+          </button>
         </div>
-    )
+      </div>
 
-
-}
+      <ul>{props.children}</ul>
+    </div>
+  );
+};
 
 export default SideDrawer;
